722d52592209e8f7bf750c6c70674bd0d2ae2d31
eedomeng <eedomeng@gmail.com>
2023-03-06 11:42:30 +0900

    Controller create

---
+++ petManager/src/main/java/com/ts/mvc/blog/BlogController.java
@@ -0,0 +1,20 @@
+package com.ts.mvc.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("blog")
+public class BlogController {
+
+	@GetMapping("guestbook")
+	public String guestbook() {
+		return "/html/guestbook";
+	}
+	
+	@GetMapping("diary")
+	public String diary() {
+		return "/html/diary";
+	}
+}
